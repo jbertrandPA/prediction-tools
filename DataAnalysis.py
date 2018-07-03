8a9d228ba241bd171e4d0c436bf4da7d2822e63b
Juliette Bertrand <j.bertrand@criteo.com>
2018-07-02 18:16:19 -0700

    Clean the notebooks

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -9,40 +9,19 @@ import calendar
 from Plotting import *
 
 
-# Compute the ratio as a percentage from a serie x
-def ComputeRatio(x):
-    return 100 * x[1]/x[0] 
 
+# returns the file name with the file path
 def get_file_path(dimensions, nb_days, table_name, with_label_number = False):
     return "files/" + get_filename(dimensions, nb_days, table_name, with_label_number)
 
+# computes the file name
 def get_filename(dimensions, nb_days, table_name, with_label_number = False):
     days = str(nb_days) + "days" if nb_days > -1 else "alldays"
     description = "labelnb" + table_name  if with_label_number else table_name
     return "_".join([description, "_".join(dimensions), days]) + ".txt"
 
 # Reads a file
-def read_and_clean(filename, label, label_aggregation, filtered_days, additional_columns = True):
-    df = pd.read_csv(filename, sep='\t', header=None)
-    if label_aggregation == "":
-        df.columns = ['Day', 'Hour', 'Count', label]
-    else:
-        df.columns = ['Day', 'Hour', label_aggregation, 'Count', label]
-    df = df.fillna("Other")
-
-    # order
-    df = df.sort_values(by=['Day', 'Hour'])
-
-    # Filter on bad day
-    df = df[~df.Day.isin(filtered_days)]
-
-    if additional_columns :
-        return compute_additional_columns(df, label)
-    else :
-        return df
-
-
-def read_and_clean2(filename, dimensions, sorting_dimensions=[], ascending=True):
+def read_and_clean(filename, dimensions, sorting_dimensions=[], ascending=True):
     df = pd.read_csv(filename, sep='\t', header=None)
     names = list(dimensions)
     names.extend(['Count', "Output"])
@@ -53,9 +32,43 @@ def read_and_clean2(filename, dimensions, sorting_dimensions=[], ascending=True)
     if len(sorting_dimensions) > 0:
         df = df.sort_values(by=sorting_dimensions, ascending=ascending)
 
-    # Compute ratio (sales or click)
-    # df['Ratio'] = df[["Count", "Output"]].apply(lambda x: ComputeRatio(x.values), axis=1)
+    return df
 
+
+## Splitting and grouping per dimensions
+
+# split into 2 df, one aggregated per day, the other per hour.
+# Note: it cannot be done with split_dimensions because it doesnt use the same group (the aggregation
+# per hour is actually on the average of the days)
+def split_day_hour(df):
+    df_by_day = group_by_label_sum(df, ["day"])
+    df_by_day = compute_ratio(df_by_day)
+    df_by_hour = group_by_label_average(df, "hour")
+    df_by_hour = compute_ratio(df_by_hour)
+    return [df_by_day, df_by_hour]
+
+# split into several data frames aggregated per each dimension
+def split_dimensions(df, dimensions,sortingLabels = "", ascending= True):
+    groups = [group_by_label_sum(df, dimension, sortingLabels, ascending) for dimension in dimensions]
+    return [compute_ratio(group) for group in groups]
+
+# aggregate the data frame by summing
+def group_by_label_sum(df, labels, sortingLabels = "", ascending= True): #, orderByLabel, )
+    df_by_label = df.groupby(labels, as_index = False).sum()
+    if len(sortingLabels) > 0 :
+        df_by_label = df_by_label.sort_values(by=sortingLabels, ascending=ascending)
+    return df_by_label
+
+# aggregate the data frame by averaging
+def group_by_label_average(df, labels, sortingLabels = "", ascending= True): #, orderByLabel, ascending)
+    df_by_label = df.groupby(labels, as_index = False).mean()
+    if len(sortingLabels) > 0 :
+        df_by_label = df_by_label.sort_values(by=sortingLabels, ascending=ascending)
+    return df_by_label
+
+# Compute the ratio as a percentage from a df
+def compute_ratio(df):
+    df['Ratio'] = df[["Count","Output"]].apply(lambda x: ComputeRatio(x.values),axis = 1)
     return df
 
 def compute_additional_columns(df):
@@ -63,39 +76,6 @@ def compute_additional_columns(df):
     df['WeekDay'] = df[["Day"]].apply(lambda x: calendar.day_name[datetime.strptime(x.values[0], '%Y-%m-%d').weekday()], axis=1)
     df['WeekHour'] = df[["WeekDay", "Hour"]].apply(lambda x: str(x.values[0]) + str(x.values[1]), axis=1)
     df['DayAndHour'] = df[["Day", "Hour"]].apply(lambda x: str(x.values[0]) + str(x.values[1]), axis=1)
-
-
-    return df
-
-def split(df, label_table):
-    df_by_day = group_by_label_sum(df, ["Day"])
-    df_by_day = compute_ratio(df_by_day, label_table)
-    df_by_hour = group_by_label_average(df, "Hour")
-    df_by_hour = compute_ratio(df_by_hour, label_table)
-    return [df_by_day, df_by_hour]
-
-def split2(df, dimensions,sortingLabels = "", ascending= True):
-    groups = [group_by_label_sum(df, dimension, sortingLabels, ascending) for dimension in dimensions]
-    return [compute_ratio2(group) for group in groups]
-
-def group_by_label_sum(df, labels, sortingLabels = "", ascending= True): #, orderByLabel, )
-    df_by_label = df.groupby(labels, as_index = False).sum()
-    if len(sortingLabels) > 0 :
-        df_by_label = df_by_label.sort_values(by=sortingLabels, ascending=ascending)
-    return df_by_label
-
-def group_by_label_average(df, labels, sortingLabels = "", ascending= True): #, orderByLabel, ascending)
-    df_by_label = df.groupby(labels, as_index = False).mean()
-    if len(sortingLabels) > 0 :
-        df_by_label = df_by_label.sort_values(by=sortingLabels, ascending=ascending)
-    return df_by_label
-
-def compute_ratio(df, ratioLabel):
-    df['Ratio'] = df[["Count",ratioLabel]].apply(lambda x: ComputeRatio(x.values),axis = 1)
-    return df
-
-def compute_ratio2(df):
-    df['Ratio'] = df[["Count","Output"]].apply(lambda x: ComputeRatio(x.values),axis = 1)
     return df
 
 def compute_percentage_volumes(df, volumeLabel):
@@ -122,32 +102,18 @@ def display(rows):
     t.add_rows(rows)
     print(t.draw())
 
-def display_rows_by_aggretor0(df,aggregator,label_table):
-    i = 0
-    rows = [["", "Mean", 'Std', "Std/Mean percent"]]
-
-    for value in df[aggregator].unique():
-        df_aggregator = df[df[aggregator] == value]
-        df_aggregator_by_day = group_by_label_sum(df_aggregator, ["Day"])
-        df_aggregator_by_day = compute_ratio(df_aggregator_by_day, label_table)
-        # print(df_aggregator_by_day["Count"].mean())
-        update_rows(rows, [(df_aggregator_by_day, "day")], "Count", str(value))
-
-    #print(rows)
-    display(rows)
-
-def display_rows_by_aggretor(df,aggregator,label_table,volume_label, values=[]):
+def display_rows_by_aggretor(df,aggregator,volume_label, values=[]):
     i = 0
     rows = [["", "Mean", 'Std', "Std/Mean percent"]]
 
     values = df[aggregator].unique() if len(values)== 0 else values
     for value in values:
         df_aggregator = df[df[aggregator] == value]
-        df_aggregator_by_day = group_by_label_sum(df_aggregator, ["Day"])
-        df_aggregator_by_day = compute_ratio(df_aggregator_by_day, label_table)
+        df_aggregator_by_day = group_by_label_sum(df_aggregator, ["day"])
+        df_aggregator_by_day = compute_ratio(df_aggregator_by_day)
 
-        df_aggregator_by_hour = group_by_label_average(df_aggregator, ["Hour"])
-        df_aggregator_by_hour = compute_ratio(df_aggregator_by_hour, label_table)
+        df_aggregator_by_hour = group_by_label_average(df_aggregator, ["hour"])
+        df_aggregator_by_hour = compute_ratio(df_aggregator_by_hour)
         update_rows(rows, [(df_aggregator_by_day, "day"), (df_aggregator_by_hour, "hour")], [volume_label], str(value))
 
     display(rows)
@@ -163,20 +129,6 @@ def update_rows(rows, tables,labels, value=""):
             rows.append(get_average_std(table, title + " per " + table_title, label))
     return rows
 
-
-
-#def get_global_rows(df_hour, df_day, labels):
-#    rows = [["", "Mean", 'Std', "Std/Mean percent"]]
-#    for label in labels:
-#        rows = update_rows(rows, df_hour, df_day, label)
-#    return rows
-
-
-#def update_rows(rows, df_hour, df_day, label, value=""):
-#    title = value + ": " + label if value != "" else label
-#    rows.append(get_average_std(df_day, title + " per day", label))
-#    rows.append(get_average_std(df_hour, title + " per hour", label))
-#    return rows
 
 # Select the top first rows
 def select_top_rows(df, nb_rows):
